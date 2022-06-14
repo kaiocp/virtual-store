bcb89e0569b6d94b9cc5ad21be4f529bba1ebd4b
Artur.Mota <arturrodriguesmota@hotmail.com>
2022-06-14 08:29:23 -0300

    Funcionalidade de pesquisa

--- Front-End/header/src/components/Search.jsx
+++ Front-End/header/src/components/Search.jsx
@@ -1,10 +1,17 @@
 import './Search.css'
+import { useState } from 'react'
 import LupaPesquisa from './imagens/LupaPesquisa.png'
 export default function Search() {
+  function pesquisaProduto(e){
+    e.preventDefault()
+    console.log(produto)
+  }
+  const [produto, setProduto] = useState()
   return (
     <div className='inputDiv'>
-      <img className='searchImg' src={LupaPesquisa}/>
-      <input className='inputSearch' placeholder='Pesquise por um produto' type="text"></input>
+      <img className='searchImg' src={LupaPesquisa} onClick={pesquisaProduto}/>
+      <input className='inputSearch' placeholder='Pesquise por um produto'
+      onChange={(e) => setProduto(e.target.value)} type="text"></input>
     </div>
   )
 }
