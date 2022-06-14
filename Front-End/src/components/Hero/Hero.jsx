51ad64487bf5decabe64c4dd3469ad25af31b9f1
kaiocp <kaiocp7@gmail.com>
2022-06-14 15:16:24 -0300

    feat: hero desktop

--- Front-End/src/components/Hero/Hero.jsx
+++ Front-End/src/components/Hero/Hero.jsx
@@ -5,13 +5,15 @@ import './hero.css';
 export default function Hero() {
     return (
         <section className='hero'>
-            <div className='hero__imgbox'>
-                <img src={iconeSacolaCompras} alt="Compras" />
-            </div>            
-            <div className='hero__info'>
-                <h1>Explore nossa nova coleção</h1>
-                <p>Aproveite as ofertas e encontre o look ideal para aproveitar o seu São João na Lojinha.</p>
-                <button>Ver descontos <img src={iconeChevronDown}/></button>
+            <div className="hero__main">
+                <div className='hero__imgbox'>
+                    <img src={iconeSacolaCompras} alt="Compras" />
+                </div>            
+                <div className='hero__info'>
+                    <h1>Explore nossa nova coleção</h1>
+                    <p>Aproveite as ofertas e encontre o look ideal para aproveitar o seu São João na Lojinha.</p>
+                    <button>Ver descontos <img src={iconeChevronDown}/></button>
+                </div>
             </div>
         </section>
     )
