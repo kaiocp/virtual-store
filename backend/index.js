58bc17802b7c0912248b9635dc5f086461ecce8f
leonardocgs <leonardocesargoes@gmail.com>
2022-06-14 17:35:22 -0300

    Fix: index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
-server.listen(3000, 'localhost')
+const port = process.env.PORT || 3000
+server.listen(port)
