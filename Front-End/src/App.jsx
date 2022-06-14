342ed7414d851b8db5d0bb1daa4b1f335e7b96c9
kaiocp <kaiocp7@gmail.com>
2022-06-14 14:45:06 -0300

    feat: hero mobile

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <div className="App">
+      <Hero />
       <Footer />
     </div>
   )
---
+++ Front-End/src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import iconeSacolaCompras from './img/compras.svg';
+import iconeChevronDown from './img/chevron-down 1.svg';
+import './hero.css';
+
+export default function Hero() {
+    return (
+        <section className='hero'>
+            <div className='hero__imgbox'>
+                <img src={iconeSacolaCompras} alt="Compras" />
+            </div>            
+            <div className='hero__info'>
+                <h1>Explore nossa nova coleção</h1>
+                <p>Aproveite as ofertas e encontre o look ideal para aproveitar o seu São João na Lojinha.</p>
+                <button>Ver descontos <img src={iconeChevronDown}/></button>
+            </div>
+        </section>
+    )
+}
