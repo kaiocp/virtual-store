d42c3402a1701fd9fa4c982bc889cbf7fa4e7811
Artur.Mota <arturrodriguesmota@hotmail.com>
2022-06-15 08:38:01 -0300

    Finalização header

--- Front-End/src/components/Header/Search.jsx
+++ Front-End/src/components/Header/Search.jsx
@@ -9,7 +9,7 @@ export default function Search() {
   const [produto, setProduto] = useState()
   return (
     <div className='inputDiv' id='inputDiv'>
-      <img className='searchImg' src={LupaPesquisa} onClick={pesquisaProduto}/>
+      <img className='searchImg' id="searchImg" src={LupaPesquisa} onClick={pesquisaProduto}/>
       <input className='inputSearch' id="inputSearch" placeholder='Pesquise por um produto'
       onChange={(e) => setProduto(e.target.value)} type="text"></input>
     </div>
