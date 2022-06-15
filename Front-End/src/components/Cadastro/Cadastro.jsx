03f8c7826ba5579452c22f56721f720777b47d09
kaiocp <kaiocp7@gmail.com>
2022-06-15 16:40:35 -0300

    feat: post on api (failing)

--- Front-End/src/components/Cadastro/Cadastro.jsx
+++ Front-End/src/components/Cadastro/Cadastro.jsx
@@ -1,8 +1,56 @@
-import iconeUploadImagem from './img/arcticons_gallery.svg';
 import '../../reset.css';
 import './cadastro.css';
+import { useState } from 'react';
 
 export default function Cadastro() {
+    const [imgUrl, setImgUrl] = useState("");
+    const [titulo, setTitulo] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [marca, setMarca] = useState("");
+    const [cor, setCor] = useState("");
+    const [categoria, setCategoria] = useState("");
+    const [subcategoria, setSubcategoria] = useState("");
+    const [preco, setPreco] = useState(0);
+    const [message, setMessage] = useState("");
+    const [messageClass, setMessageClass] = useState("");
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch("https://sleepy-cliffs-93443.herokuapp.com/products", {
+                method: "POST",
+                body: JSON.stringify({
+                    "product_image_url": imgUrl,
+                    "product_title": titulo,
+                    "product_discription": descricao,
+                    "product_brand": marca,
+                    "product_color": cor,
+                    "product_category": categoria,
+                    "product_subcategory": subcategoria,
+                    "product_price": preco
+                    })
+                }
+            );
+            if (res.status === 200) {
+                setImgUrl("");
+                setTitulo("");
+                setDescricao("");
+                setMarca("");
+                setCor("");
+                setCategoria("");
+                setSubcategoria("");
+                setPreco("");
+                setMessage("Produto cadastrado com sucesso!");
+                setMessageClass("sucesso");
+            } else {
+                setMessage("Houve um erro ao cadastrar seu produto.");
+                setMessageClass("erro");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }    
+
     return (
         <section className='cadastro'>
             <div className='cadastro__header'>
@@ -11,39 +59,67 @@ export default function Cadastro() {
             </div>
 
             <fieldset>
-                <form action="" className='cadastro__form'>
+                <form onSubmit={handleSubmit} className='cadastro__form'>
                     <div className='cadastro__form--img'>
                         <label htmlFor="imagemProduto">Preview da imagem</label>
                         <div className='caixa' onClick={(ev) => {
                                 ev.preventDefault();
                                 document.getElementById('img').focus()}} >
-                            <img src={iconeUploadImagem} alt="" id="imagemProduto" />
+                            <img src={imgUrl ? imgUrl : null} alt="" id="imagemProduto" />
                         </div>                
                     </div>
 
                     <div className='cadastro__form--fields'>
                         <div className='field'>
                             <label className='label' htmlFor="">Url da imagem</label>
-                            <input className="field--input" type="text" id="img"/>
+                            <input 
+                                className="field--input" 
+                                type="text" 
+                                id="img"
+                                value={imgUrl}
+                                onChange={(e) => setImgUrl(e.target.value)} 
+                            />
                         </div>
                         <div className='field'>
                             <label className='label' htmlFor="">Título</label>
-                            <input className="field--input" type="text" name="" id="" />
+                            <input 
+                                className="field--input" 
+                                type="text" 
+                                value={titulo}
+                                onChange={(e) => setTitulo(e.target.value)} 
+                            />
                         </div>
                         <div className='field'>
                             <label className='label' htmlFor="" id="d">Descrição do produto</label>
-                            <textarea className="field--input" type="text" name="" id="f" />
+                            <textarea 
+                                className="field--input" 
+                                type="text"
+                                id="f" 
+                                maxLength="300"
+                                value={descricao}
+                                onChange={(e) => setDescricao(e.target.value)} 
+                            />
                             <p className="field--maxchar" id="e">(max. 300 caracteres)</p>
                         </div>
 
                         <div className='field--marca-cor'>
                             <div className='field field-2'>
                                 <label className='label' htmlFor="">Marca</label>
-                                <input className="field--input" type="text" name="" id="" />
+                                <input 
+                                    className="field--input" 
+                                    type="text"
+                                    value={marca}
+                                    onChange={(e) => setMarca(e.target.value)}  
+                                />
                             </div>
                             <div className='field field-3'>
                                 <label className='label' htmlFor="">Cor</label>
-                                <input className="field--input" type="text" name="" id="" />
+                                <input 
+                                    className="field--input" 
+                                    type="text"
+                                    value={cor}
+                                    onChange={(e) => setCor(e.target.value)} 
+                                />
                             </div>
                         </div>
 
@@ -51,32 +127,51 @@ export default function Cadastro() {
                             <div className="field--categorias field-2">
                                 <div className='field'>
                                     <label className='label' htmlFor="category">Categoria</label>
-                                    <select className="field--input field--dropwdown" list="category" name="Categoria" for="category">
-                                        <option defaultValue="" disabled selected></option>
-                                        <option value="Red">Red</option>
-                                        <option value="Green">Green</option>
-                                        <option value="Blue">Blue</option>
+                                    <select 
+                                        className="field--input field--dropwdown"
+                                        value={categoria}
+                                        onChange={(e) => setCategoria(e.target.value)}                                  
+                                    >
+                                        <option disabled />
+                                        <option value="Camisa">Camisa</option>
+                                        <option value="Calça">Calça</option>
+                                        <option value="Bolsa">Bolsa</option>                                
                                     </select>
                                 </div>
                                 <div className='field'>
                                     <label className='label' htmlFor="subcategory">Subcategoria</label>
-                                    <select className="field--input field--dropwdown" list="subcategory" name="Subcategoria" for="subcategory">
-                                        <option defaultValue="" disabled selected></option>
-                                        <option value="Red">Red</option>
-                                        <option value="Green">Green</option>
-                                        <option value="Blue">Blue</option>
+                                    <select 
+                                        className="field--input field--dropwdown" 
+                                        value={subcategoria}
+                                        onChange={(e) => setSubcategoria(e.target.value)}                                  
+                                    >
+                                        <option disabled />
+                                        <option value="P">P</option>
+                                        <option value="M">M</option>
+                                        <option value="G">G</option>
+                                        <option value="GG">GG</option>
                                     </select>
                                 </div>  
                             </div>
                             <div className='field field-2'>
                                 <label className='label' htmlFor="">Preço</label>
-                                <input className="field--input" type="number" min="0.00" step="0.01" />
+                                <input 
+                                    className="field--input" 
+                                    type="number" 
+                                    min="0.00" 
+                                    step="0.01"
+                                    value={preco}
+                                    onChange={(e) => setPreco(e.target.value)} 
+                                />
                             </div>                    
                         </div>
                         <div className='cadastro__form--btn'>
-                        <button>
-                            Cadastrar produto
-                        </button>
+                            <button type="submit">
+                                Cadastrar produto
+                            </button>
+                            <div className="message">
+                                {message ? <p className={messageClass}>{message}</p> : null}
+                            </div>
                     </div>
                     </div>
                 </form>
