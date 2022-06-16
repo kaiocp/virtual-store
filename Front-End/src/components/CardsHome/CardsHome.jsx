c3096f83b2ed42ac0ad74f0ef13f7334aaa44c1d
Artur.Mota <arturrodriguesmota@hotmail.com>
2022-06-16 19:43:26 -0300

    Puxando imagem dos produtos

--- Front-End/src/components/CardsHome/CardsHome.jsx
+++ Front-End/src/components/CardsHome/CardsHome.jsx
@@ -1,13 +1,21 @@
 import RetanguloFundo from './imagens/RetanguloFundo.png'
+import Camisa from './imagens/Camisa.png'
+import Tenis from './imagens/Tenis.png'
+import Bolsa from './imagens/Bolsa.png'
 import styles from './CardsHome.module.css'
 export default function CardsHome() {
   return (
-    <div className={styles.cardsHome}>
-      <div className={styles.produtosCards}>
+    <section className={styles.sectionCards}>
+      <div className={styles.produtosImagens}>
+        <img src={Camisa}/>
+        <img src={Tenis}/>
+        <img src={Bolsa}/>
+      </div>
+      <div className={styles.cardsImagens}>
         <img src={RetanguloFundo}/>
         <img src={RetanguloFundo}/>
         <img src={RetanguloFundo}/>
       </div>
-    </div>
+    </section>
   )
 }
