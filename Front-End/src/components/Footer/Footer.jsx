365ca874112dff5bfa5a824b7f106ecec71ac969
kaiocp <kaiocp7@gmail.com>
2022-06-16 17:23:20 -0300

    fix: css modules

--- Front-End/src/components/Footer/Footer.jsx
+++ Front-End/src/components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import iconeFacebook from './img/facebook.svg';
 import iconeTwitter from './img/twitter.svg';
 import iconeInstagram from './img/instagram.svg';
 import '../../reset.css';
-import './footer.css';
+import styles from  './footer.module.css';
 
 
 export default function Footer() {
     return (
-    <footer className='footer'>
-        <div className='footer__main'>
-            <div className='footer__links' id='b'>
+    <footer className={styles.footer}>
+        <div className={styles.footer__main}>
+            <div className={styles.footer__links} id={styles.b}>
                 <ul>
                     <li><a href=''>Início</a></li>
                     <li><a href=''>Produtos</a></li>
@@ -18,11 +18,11 @@ export default function Footer() {
                     <li><a href=''>Carrinho</a></li>
                 </ul>
             </div>
-            <div className='footer__lojinha' id='a'>
+            <div className={styles.footer__lojinha} id={styles.a}>
                 <img src={iconeSacolaLojinha} alt="Lojinha" />
                 <p>Lojinha</p>
             </div>
-            <div className='footer__social' id='c'>
+            <div className={styles.footer__social} id={styles.c}>
                 <ul>
                     <li><a href="" target="_blank"><img src={iconeFacebook} alt='Facebook'/></a></li>
                     <li><a href="" target="_blank"><img src={iconeTwitter} alt='Twitter'/></a></li>
@@ -30,7 +30,7 @@ export default function Footer() {
                 </ul>
             </div>
         </div>
-        <div className='footer__message'>
+        <div className={styles.footer__message}>
             <p>Feito pela <a href='https://infojr.com.br/' target="_blank">Infojr UFBA</a> com Figma, React e muito <span role="img" aria-label="green-heart">💚</span></p>
         </div>
     </footer>
