7d2334e72d61e9f0cb756a24519d404bd3337b03
Artur.Mota <arturrodriguesmota@hotmail.com>
2022-06-17 11:58:01 -0300

    Responsivo Desktop

--- Front-End/src/components/CardsHome/Card.jsx
+++ Front-End/src/components/CardsHome/Card.jsx
@@ -1,5 +1,6 @@
 import styles from './Card.module.css'
 import RetanguloFundo from './imagens/RetanguloFundo.png'
+import RetanguloFundoDesktop from './imagens/RetanguloFundoDesktop.png'
 import SetaVerMais from './imagens/SetaVerMais.png'
 export default function Card(props) {
   return (
@@ -11,6 +12,7 @@ export default function Card(props) {
         <img className={styles.setaVerMais} src={SetaVerMais}/>
       </div>
       <img className={styles.imgRetangulo} src={RetanguloFundo}/>
+      <img className={styles.imgRetanguloDesktop} src={RetanguloFundoDesktop}/>
     </div>
   )
 }
