9d6a87a688ff258a8f974acbeb4b573479d7bfce
kaiocp <kaiocp7@gmail.com>
2022-06-18 15:00:47 -0300

    refactor: header styles

--- Front-End/src/components/Header/Header.jsx
+++ Front-End/src/components/Header/Header.jsx
@@ -7,20 +7,20 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className={styles.headerDiv} id={styles.headerDiv}>
-      <div className={styles.headerEsquerda} id={styles.headerEsquerda}>
-        <div className={styles.logoLojinha} id={styles.logoLojinha}>
+    <header className={styles.headerDiv}>
+      <div className={styles.headerEsquerda}>
+        <div className={styles.logoLojinha}>
           <Link to="/"><img src={LogoLojinha}/></Link>
         </div>
-        <div className={styles.searchDivDesk} id={styles.searchDivDesk}>
+        <div className={styles.searchDivDesk}>
           <Search/>
         </div>
-        <img id={styles.menuHamburguer} src={MenuHamburguer}/>
-        <div className={styles.navbarDiv} id={styles.navbarDiv}>
+        <img className={styles.menuHamburguer} src={MenuHamburguer}/>
+        <div className={styles.navbarDiv}>
           <Navbar/>
         </div>
       </div>
-      <div className={styles.searchDiv} id={styles.searchDiv}>
+      <div className={styles.searchDiv}>
         <Search/>
       </div>
     </header>
--- Front-End/src/components/Header/Search.jsx
+++ Front-End/src/components/Header/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './Search.module.css';
 import styles2 from './Header.module.css'
 import LupaPesquisa from './imagens/LupaPesquisa.svg';
