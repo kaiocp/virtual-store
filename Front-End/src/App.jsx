1b63d36ddbc9c1f273bdebbf8fe775f3afda305b
kaiocp <kaiocp7@gmail.com>
2022-06-18 16:51:05 -0300

    refactor: products page

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Home from './pages/Home';
 import Anunciar from './pages/Anunciar';
 import SearchResults from './components/SearchResults/SearchResults';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />}></Route>
           <Route path='/anunciar' element={<Anunciar />}></Route>
+          <Route path='/produtos' element={<Products />}></Route>
           <Route path='/pesquisa/:query' element={<SearchResults />}></Route>
         </Routes>
 
--- Front-End/src/pages/Home.jsx
+++ Front-End/src/pages/Home.jsx
@@ -1,54 +1,21 @@
 import Hero from "../components/Hero/Hero";
-import ProductsGrid from "../components/ProductsGrid/ProductsGrid";
-import ProductCard from "../components/ProductCard/ProductCard";
-import axios from 'axios';
-import { useEffect, useState, useRef } from "react";
+import { useRef } from 'react';
+import Products from "./Products";
 
 export default function Home() {
-    const [products, setProducts] = useState({});
-    const [loading, setLoading] = useState(true);
 
     const myRef = useRef(null)
 
     const executeScroll = () => myRef.current.scrollIntoView();
 
-    useEffect(() => {
-        let url = "https://sleepy-cliffs-93443.herokuapp.com/products/";
-        const fetchData = async () => {
-        setLoading(true);
-        try {
-            const {data: response} = await axios.get(url);
-            setProducts(response);
-        } catch (error) {
-            console.error(error.message);
-        }
-        setLoading(false);
-        }
-        fetchData();
-    }, []);
-
     return (
         <>
             <Hero func={executeScroll} />
-            {!loading && (
+
                 <div ref={myRef}>
-                    <ProductsGrid
-                    pageTitle='Produtos'
-                    card={
-                        products.content.map(el => (
-                            <ProductCard
-                                key={el.prod_id}
-                                img={el.prod_image_url}
-                                subcategory={el.subcategory_name}
-                                price={el.prod_price}
-                                title={el.prod_title}
-                                category={el.category_name}
-                            />
-                        ))
-                        }
-                    />
+                    <Products />
                 </div>
-            )}
+
         </>
     )
 }
