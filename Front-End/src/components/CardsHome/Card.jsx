bdc2da384d9f9e276d1b92d14d26c1c4d169bd50
João Pedro de Menezes Pontes Fernandes <joao.pedro.fernandes.122@gmail.com>
2022-06-19 11:39:59 -0300

    fix home cards

--- Front-End/src/components/CardsHome/Card.jsx
+++ Front-End/src/components/CardsHome/Card.jsx
@@ -4,19 +4,26 @@ import RetanguloFundoDesktop from './imagens/RetanguloFundoDesktop.png'
 import SetaVerMais from './imagens/SetaVerMais.svg'
 export default function Card(props) {
   return (
-    <div className={styles.cardsDiv}>
-      <div className={styles.cardsImagens}>
+    <article className={styles.card}>
+      <img className={styles.imagemProduto} src={props.imagem} alt="" />
+      <div className={styles.textDiv}>
+        <h1 className={styles.produtoType}>{props.texto}</h1>
+        <p className={styles.vermais}>Ver mais &gt;</p>
+      </div>
+    </article> 
+    )
+}
+   /*  <div className={styles.cardsDiv}>
+     <div className={styles.cardsImagens}>
         <img className={styles.imgRetanguloDesktop} src={RetanguloFundoDesktop}/>
         <img className={styles.imgRetangulo} src={RetanguloFundo}/>
-      </div>
+      </div> 
       <img className={styles.imgProduto} src={props.imagem}/>
       <div className={styles.textoCards}>
         <h1 className={styles.textoProduto}>{props.texto}</h1>
-        <div className={styles.verMais}>
+         <div className={styles.verMais}> 
           <p className={styles.textoVerMais}>Ver mais</p>
           <img className={styles.setaVerMais} src={SetaVerMais}/>
-        </div>
+         </div> 
       </div>
-    </div>
-  )
-}
+    </div> */
