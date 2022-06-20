f0ad6494dc18b1d99d66f821917226493f80ed20
Artur.Mota <arturrodriguesmota@hotmail.com>
2022-06-20 16:29:37 -0300

    Inicio modal do Produto

---
+++ Front-End/src/components/modalProduto/ModalProduto.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from './ModalProduto.module.css'
+export default function ModalProduto() {
+  return (
+    <section>
+      <h1>teste</h1>
+    </section>
+  )
+}
--- Front-End/src/pages/Home.jsx
+++ Front-End/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import CardsHome from "../components/CardsHome/CardsHome";
 import Hero from "../components/Hero/Hero";
+import ModalProduto from "../components/modalProduto/modalProduto";
 
 export default function Home() {
     return (
         <>
             <Hero />
             <CardsHome />
+            <ModalProduto/>
         </>
     )
 }
