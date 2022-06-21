9decfbda91e5aa4806faae7056c99009228efb4f
kaiocp <kaiocp7@gmail.com>
2022-06-21 12:58:43 -0300

    feat: carrinho empty mobile and tablet

---
+++ Front-End/src/components/Carrinho/Carrinho.jsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import ProductCard from "../ProductCard/ProductCard";
+import setaVetor from './img/setaVetor.svg';
+import styles from './Carrinho.module.css';
+
+
+export default function Carrinho () {
+
+    const [loading, setLoading] = useState(true);
+    const [vistoRecentementeData, setVistoRecentementeData] = useState({
+        key: '',
+        img: '',
+        subcategory: '',
+        price: '',
+        title: '',
+        category: ''
+    });
+
+    useEffect(() => {
+        let url = `https://sleepy-cliffs-93443.herokuapp.com/products/`;
+        const fetchData = async () => {
+        setLoading(true);
+        try {
+            const {data: response} = await axios.get(url);
+            setVistoRecentementeData({
+                ...vistoRecentementeData,
+                key: response.content[0].prod_id,
+                img: response.content[0].prod_image_url,
+                subcategory: response.content[0].subcategory_name,
+                price: response.content[0].prod_price,
+                title: response.content[0].prod_title,
+                category: response.content[0].category_name
+            });
+        } catch (error) {
+            console.error(error.message);
+        }
+        setLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        // Implementar regra para carrinho vazio ou cheio
+        <section className={styles.carrinho}>
+            <div className={styles.carrinho__header}>
+                <img src={setaVetor}/>
+                <p>Carrinho</p>
+            </div>
+            
+            {/* começo carrinho vazio */}
+            <article className={styles.carrinho__article}>
+                <h2>Seu carrinho está vazio :(</h2>
+                <h3><Link to="/" className={styles.underline}>Continue navegando</Link> pela Lojinha e encontre produtos incríveis!</h3>
+            </article>
+            {/* fim carrinho vazio */}
+            <section className={styles.carrinho__vistoRecentemente}>
+                <p>Visto recentemente</p>
+                {loading && <h2 style={{
+                    textAlign: 'center'}}>
+                    Carregando...</h2>}
+                {!loading &&
+                    <div>
+                        <ProductCard
+                            key={vistoRecentementeData.key}
+                            img={vistoRecentementeData.img}
+                            subcategory={vistoRecentementeData.subcategory}
+                            price={vistoRecentementeData.price}
+                            title={vistoRecentementeData.title}
+                            category={vistoRecentementeData.category}
+                        />
+                    </div>
+                }
+            </section>
+        </section>
+
+
+    )
+}
