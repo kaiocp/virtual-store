c71ce793298d5eaa4d75aa224d98495270dee46d
João Pedro de Menezes Pontes Fernandes <joao.pedro.fernandes.122@gmail.com>
2022-06-20 21:33:39 -0300

    colocando os cards na home

--- Front-End/src/pages/Home.jsx
+++ Front-End/src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
     return (
         <>
             <Hero func={executeScroll} />
-
+            <CardsHome />
                 <div ref={myRef}>
                     <Products />
                 </div>
