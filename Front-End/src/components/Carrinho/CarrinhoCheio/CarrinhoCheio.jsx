76783f30351228950bd2509b3f34711e01387e49
kaiocp <kaiocp7@gmail.com>
2022-06-22 17:02:34 -0300

    feat: carrinho cheio desktop

--- Front-End/src/components/Carrinho/CarrinhoCheio/CarrinhoCheio.jsx
+++ Front-End/src/components/Carrinho/CarrinhoCheio/CarrinhoCheio.jsx
@@ -43,76 +43,73 @@ export default function CarrinhoCheio() {
 
     return (
         <section className={styles.carrinhoCheio}>
-            <section className={styles.carrinhoCheio__sec1}>
-                <div className={styles.header}>
-                    <img src={setaVetor}/>
-                    <p>Carrinho</p>
-                </div>
-                <article className={styles.articleCep}>
-                    <div className={styles.titulo}>
-                        <img src={iconeMapa}/>
-                        <h1 className={styles.tituloCep}>Buscar CEP</h1>
-                    </div>
-                    <div className={styles.buscarDiv}>
-                        <input type="text" placeholder='Digite um CEP...' className={styles.inputCep} id="cep" onChange={(e) => setCep(e.target.value)}/>
-                        <p className={styles.aplicar} onClick={buscarCep}>Aplicar</p>
-                    </div>
-                </article>
+            <div className={styles.header}>
+                <img src={setaVetor}/>
+                <p>Carrinho</p>
+            </div>
+            <div className={styles.carrinhoCheioDesk}>
+                <section className={styles.carrinhoCheio__sec1}>
+                    <article className={styles.articleCep}>
+                        <div className={styles.titulo}>
+                            <img src={iconeMapa}/>
+                            <h1 className={styles.tituloCep}>Buscar CEP</h1>
+                        </div>
+                        <div className={styles.buscarDiv}>
+                            <input type="text" placeholder='Digite um CEP...' className={styles.inputCep} id="cep" onChange={(e) => setCep(e.target.value)}/>
+                            <p className={styles.aplicar} onClick={buscarCep}>Aplicar</p>
+                        </div>
+                    </article>
 
-                <hr className={styles.hr}/>
+                    <hr className={styles.hr}/>
 
-                <section className={styles.cardsCarrinho}>
-                {!loading &&
-                    carrinhoData.content.products.map(el => (
-                        <CardCarrinho
-                            key={el.prod_id}
-                            img={el.prod_image_url}
-                            title={el.prod_title}
-                            color={el.color_name}
-                            size={el.subcategory_name}
-                            brand={el.brand_name}
-                            qtd={el.prod_total}
-                            onChange={(e) => setQtd(e.target.value)}
-                            price={el.prod_price}
-                            // decreaseQt={el.}
-                            // increaseQtd={el.}
-                            delete={() => console.log('deletou')}
-                        />
-                    ))}
+                    <section className={styles.cardsCarrinho}>
+                        {!loading &&
+                            carrinhoData.content.products.map(el => (
+                                <CardCarrinho
+                                    key={el.prod_id}
+                                    img={el.prod_image_url}
+                                    title={el.prod_title}
+                                    color={el.color_name}
+                                    size={el.subcategory_name}
+                                    brand={el.brand_name}
+                                    qtd={el.prod_total}
+                                    onChange={(e) => setQtd(e.target.value)}
+                                    price={el.prod_price}
+                                    // decreaseQt={el.}
+                                    // increaseQtd={el.}
+                                    delete={() => console.log('deletou')}
+                                />
+                            ))}
+                    </section>
+
                 </section>
 
-            </section>
 
+                <section className={styles.carrinhoCheio__sec2}>
+                    <h3>Resumo do pedido</h3>
+                    <hr className={styles.hr}/>
+                    {!loading && 
+                    <>
+                    <div className={styles.resumo}>
+                        <h4>Subtotal ({carrinhoData.content.cart_prod_total} {carrinhoData.content.cart_prod_tota === 1 ? 'item' : 'itens'})</h4>
+                        <p>R$ {carrinhoData.content.cart_subtotal}</p>
+                    </div>
 
-            <section className={styles.carrinhoCheio__sec2}>
-            <h3>Resumo do pedido</h3>
-            <hr className={styles.hr}/>
-            {!loading && 
-            <>
-            <div className={styles.resumo}>
-                <h4>Subtotal ({carrinhoData.content.cart_prod_total} {carrinhoData.content.cart_prod_tota === 1 ? 'item' : 'itens'})</h4>
-                <p>R$ {carrinhoData.content.cart_subtotal}</p>
+                    <div className={styles.resumo}>
+                        <h4>Entrega</h4>
+                        <p>R$ {cepData ? cepData?.content?.shipping_cost : '--'}</p>
+                    </div>
+                    <hr className={styles.hr}/>
+                    <div className={`${styles.resumo} ${styles.resumo_total}`}>
+                        <h4>Total</h4>
+                        <p>R$ {carrinhoData.content.cart_subtotal}</p>
+                    {/* somar com entrega (?metodo pra atualizar tudo?) */}
+                    </div>
+                    <button className={styles.botaoFinalizar}>Finalizar compra</button>
+                    </>
+                }
+                </section>
             </div>
-            {cepData ? 
-                <div className={styles.resumo}>
-                    <h4>Entrega</h4>
-                    <p>R$ {cepData?.content?.shipping_cost}</p>
-                </div>
-                :
-                null
-            }
-            <hr className={styles.hr}/>
-            <div className={`${styles.resumo} ${styles.resumo_total}`}>
-                <h4>Total</h4>
-                <p>R$ {carrinhoData.content.cart_subtotal}</p>
-                {/* somar com entrega (?metodo pra atualizar tudo?) */}
-            </div>
-            <button class={styles.botaoFinalizar}>Finalizar compra</button>
-            </>
-            }
-            </section>
-
-
         </section> 
     )
 
