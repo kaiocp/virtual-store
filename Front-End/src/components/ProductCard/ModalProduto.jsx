bed485d477845b8438b0b48ffa13e488aad90b2d
Artur.Mota <arturrodriguesmota@hotmail.com>
2022-06-24 11:01:22 -0300

    Contador de quantidade funcionando

--- Front-End/src/components/ProductCard/ModalProduto.jsx
+++ Front-End/src/components/ProductCard/ModalProduto.jsx
@@ -6,7 +6,7 @@ import IconeAdicionar from './img/iconeadicionar.svg'
 import IconeEditar from './img/iconeeditar.svg'
 import IconeDeletar from './img/iconedeletar.svg'
 import { Link } from 'react-router-dom';
-import FormEdicao from "../FormEdicao/FormEdicao"
+import { useState } from "react"
 export default function ModalProduto({onClose = () => {},titulo,imagem,desc,marca,preco,cor,tamanho,id}) {
   async function deletarProduto() {
     alert("O produto " + titulo + " foi excluido com sucesso")
@@ -18,6 +18,14 @@ export default function ModalProduto({onClose = () => {},titulo,imagem,desc,marc
   function clickEditar() {
     document.body.style.overflow = "auto";
   }
+  function removerProduto() {
+    if (qtd == 1){
+      alert("O produto " + titulo + " já está com a quantidade mínima")
+    }else{
+      setQtd(qtd - 1)
+    }
+  }
+  const [qtd, setQtd] = useState(1)
   return (
     <div className={styles.fundoModal}>
       <section className={styles.sectionModalProduto} id="modal">
@@ -32,9 +40,9 @@ export default function ModalProduto({onClose = () => {},titulo,imagem,desc,marc
             <img src={imagem}/>
             <h1 className={styles.tamanho}>{tamanho}</h1>
             <div className={styles.quantidade}>
-              <img src={IconeRemover}/>
-              <h1>1</h1>
-              <img src={IconeAdicionar}/>
+              <img src={IconeRemover} onClick={removerProduto}/>
+              <h1>{qtd}</h1>
+              <img src={IconeAdicionar} onClick={() => setQtd(qtd + 1)}/>
             </div>
           </header>
           <main>
